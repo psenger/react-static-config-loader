5faa19b0a9e33e838a9b4ab10b5f7cae74d81390
Philip Senger <philip@datameshgroup.com>
2022-09-20 10:10:30 +1000

    Updates

---
+++ precache-manifest.218a78d187b4ffd52d5c7e5982e20a10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40500132ceabc7c4bab8f5c5430970be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f07d7eded627fda909a",
+    "url": "./static/js/2.0d95f503.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0d95f503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055ec31915c825b50805",
+    "url": "./static/js/main.e5957bad.chunk.js"
+  },
+  {
+    "revision": "69ea49f229ffea154c88",
+    "url": "./static/js/runtime-main.9ce95253.js"
+  }
+]);
